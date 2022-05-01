a95422fc734669b818cf7c54a60edfdc116fa99e
yjara v <70939994+yjara@users.noreply.github.com>
2022-05-01 12:46:39 +0200

    Update app.py

--- afvink1/app.py
+++ afvink1/app.py
@@ -19,9 +19,7 @@ app = Flask(__name__)
 
 
 @app.route('/',methods=["POST", "GET"])
-def hello_world():  # put application's code here
-
-    # return '<marquee><a href="http://han.nl">Hello World!</marquee>' #beweging van tekst van rechts naar links<marquee>Hello World!</marquee> En a href is voor een link
+def hello_world():
     if request.method == "POST":
         answer = request.form.get("answer", "")
         output = searchDis(answer)
@@ -29,16 +27,6 @@ def hello_world():  # put application's code here
         return render_template("page1.html",output=output, answer=answer, size=len(output))
     else:
         return render_template("page1.html", output="",answer="",size=0)
-
-@app.route('/show',methods=['GET','POST']) #data via url krijgen en sturen vgm. nu gebruiken we dus get
-def show():
-    kleur = request.args.get('kleur') #http://127.0.0.1:5000/show?kleur=green
-    return '''<body bgcolor="%s"> De kleur is 
-    <form method="get">
-    <input type="text" name="kleur">
-    <input type="submit">
-    </form>
-    </body>''' % kleur #control u om html op site te zien
 
 def searchPubmed():
     fig, ax =plt.subplots(figsize=(6,4))
@@ -53,13 +41,6 @@ def searchPubmed():
         handle = Entrez.esearch(db='pubmed', term='retina',mindate=f'{date}/01/01',maxdate=f'{date+4}/12/31')
         record = Entrez.read(handle)
         handle.close()
-       # handle2 = Entrez.esearch(db='pubmed', term='testosterone',
-       #                         mindate=f'{date}/01/01',
-       #                         maxdate=f'{date + 4}/12/31')
-      #  record2 = Entrez.read(handle2)
-      #  handle2.close()
-
-      #  pubMedDate2[f'{date}-{date + 4}'] = int(record2.get('Count'))
         pubMedDate[f'{date}-{date+4}'] = int(record.get('Count'))
         date = date + 4
 
@@ -67,22 +48,7 @@ def searchPubmed():
     ax.bar(pubMedDate.keys(),pubMedDate.values(),color='r')
     plt.xticks(rotation=30,size=5)
     plt.ylabel("let's see",size=5)
-
-    # plotting the points
-    #####plt.plot(pubMedDate.keys(), pubMedDate.values(),color='r', label='retina')
-  #  plt.plot(pubMedDate2.keys(), pubMedDate2.values(), color='g', label='testos')
-    # naming the x axis
-   ##### plt.xlabel('x - axis')
-    # naming the y axis
-   ##### plt.ylabel('y - axis')
-
-    # giving a title to my graph
-    ######plt.title('My first graph!')
-    ######plt.show()
     output = io.BytesIO()
-
-
-
 
 def searchDis(test):
     cursor = conn.cursor()
