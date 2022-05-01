f17505330ef55ba530a6247adb817066e9a67f4f
yjara v <70939994+yjara@users.noreply.github.com>
2022-05-01 14:52:50 +0200

    Update afvink3.py

--- afvink3.py
+++ afvink3.py
@@ -2,42 +2,36 @@ from Bio import Entrez
 import matplotlib.pyplot as plt
 
 def searchPubmed():
+    listCompounds = openFiles().split("\n")
     pubMedDate = {}
-    pubMedDate2 = {}
+    Entrez.email = 'A.N.Other@example.com'
 
-    Entrez. = 'A.N.Other@example.com'
-
-    startdate = 1970
-    enddate = 2020
+    startdate = 2010
     #https://biopython.org/docs/1.75/api/Bio.Entrez.html
 
     date = startdate
-    while date < enddate:
-        handle = Entrez.esearch(db='pubmed', term='retina',mindate=f'{date}/01/01',maxdate=f'{date+4}/12/31')
+    bestCombie=""
+    biggestHit=0
+    for item in listCompounds:
+        combie = f'{item} AND Alzheimer'
+        handle = Entrez.esearch(db='pubmed', term=combie,
+                                mindate=f'{date}/01/01')
         record = Entrez.read(handle)
         handle.close()
-        handle2 = Entrez.esearch(db='pubmed', term='testosterone',
-                                mindate=f'{date}/01/01',
-                                maxdate=f'{date + 4}/12/31')
-        record2 = Entrez.read(handle2)
-        handle2.close()
+        countCombo = int(record.get('Count'))
+        if countCombo > biggestHit:
+            biggestHit = countCombo
+            bestCombie = combie
+    print(bestCombie)
+    print(biggestHit)
 
-        pubMedDate2[f'{str(date)[2:4]}-{str(date + 5)[2:4]}'] = int(record2.get('Count'))
-        pubMedDate[f'{str(date)[2:4]}-{str(date+5)[2:4]}'] = int(record.get('Count'))
-        date = date + 5
-
-    # plotting the points
-    plt.plot(pubMedDate.keys(), pubMedDate.values(),='r', label='retina')
-    plt.plot(pubMedDate2.keys(), pubMedDate2.values(), color='g', label='testos')
-    # naming the x axis
-    plt.xlabel('jaar')
-    plt.xticks(rotation=90,fontsize=6)
-    # naming the y axis
-    plt.ylabel('hits')
-    # giving a title to  graph
-    plt.title(f'Hits per 5 jaar, van {startdate} tot {enddate}')
-    # function to  the plot
-    plt.show()
+def openFiles():
+    compoundsFileName ="c_oef.txt"
+    geneFileName ="textmining/week 3/g_oef.txt"
+    moleculairFileName = "textmining/week 3/m_oef.txt"
+    compoundsFile = open(compoundsFileName,'r')
+    compoundsList = compoundsFile.read()
+    return compoundsList
 
 if __name__ == '__main__':
     searchPubmed()
